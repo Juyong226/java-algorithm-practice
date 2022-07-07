adc6ca2effd24d246f58e7ba49c778afab408ed8
Juyong226 <juyong226@naver.com>
2022-07-07 20:04:27 +0900

    HashMap, TreeSet 3

---
+++ src/HashMapAndTreeSet/CountAllAnagram.java
@@ -0,0 +1,50 @@
+package HashMapAndTreeSet;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class CountAllAnagram {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String pool = scanner.next();
+        String target = scanner.next();
+        System.out.println(CountAllAnagram.solution(pool, target));
+    }
+
+    private static int solution(String pool, String target) {
+        int answer = 0;
+
+        HashMap<Character, Integer> original = new HashMap<>();
+        HashMap<Character, Integer> temp_map = new HashMap<>();
+
+        char temp;
+        for (int i=0; i<target.length(); i++) {
+            temp = target.charAt(i);
+            if (original.containsKey(temp)) original.replace(temp, original.get(temp) + 1);
+            else original.put(temp, 1);
+
+            temp = pool.charAt(i);
+            if (temp_map.containsKey(temp)) temp_map.replace(temp, temp_map.get(temp) + 1);
+            else temp_map.put(temp, 1);
+        }
+
+        int lp = 0;
+        char left, right;
+        for (int rp=target.length(); rp<pool.length(); rp++) {
+            if (original.equals(temp_map)) answer++;
+            left = pool.charAt(lp);
+            right = pool.charAt(rp);
+            if (temp_map.containsKey(left)) {
+                if ((temp_map.get(left) - 1) == 0) temp_map.remove(left);
+                else temp_map.replace(left, temp_map.get(left) - 1);
+            }
+            if (temp_map.containsKey(right)) temp_map.replace(right, temp_map.get(right) + 1);
+            else temp_map.put(right, 1);
+            lp++;
+        }
+        if (original.equals(temp_map)) answer++;
+
+        return answer;
+    }
+}
