82884c948c5de81fb45e4e130903079e98ecb821
Juyong226 <juyong226@naver.com>
2023-02-01 00:26:12 +0900

    HashMap and TreeSet 8

--- src/HashMapAndTreeSet/TheKrdBiggestNumber.java
+++ src/HashMapAndTreeSet/TheKrdBiggestNumber.java
@@ -1,8 +1,6 @@
 package HashMapAndTreeSet;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class TheKrdBiggestNumber {
 
@@ -14,7 +12,7 @@ public class TheKrdBiggestNumber {
         for (int i=0; i<n; i++) {
             input[i] = scanner.nextInt();
         }
-        System.out.println(TheKrdBiggestNumber.solution(n, k, input));
+        System.out.println(TheKrdBiggestNumber.solution2(n, k, input));
     }
 
     private static int solution(int n, int k, int[] input) {
@@ -48,6 +46,35 @@ public class TheKrdBiggestNumber {
         Arrays.sort(keys);
         if (keys.length < k) answer = -1;
         else answer = (int) keys[keys.length - k];
+
+        return answer;
+    }
+
+    private static int solution2 (int n, int k, int[] input) {
+        int answer = 0;
+
+        HashMap<Integer, Integer> sum = new HashMap<Integer, Integer>();
+
+        int rp = 1;
+        int i, ts;
+        for (int lp=0; lp<input.length-2; lp++) {
+            rp = lp + 1;
+            i = rp + 1;
+            while (rp < input.length-1) {
+                ts = input[lp] + input[rp] + input[i];
+                sum.put(ts, sum.getOrDefault(ts, 0) + 1);
+                i++;
+                if (i > input.length-1) {
+                    rp++;
+                    i = rp + 1;
+                }
+            }
+        }
+
+        Object[] keys = sum.keySet().toArray();
+        Arrays.sort(keys);
+        if (keys.length < k) answer = -1;
+        else answer = (int)keys[keys.length-k];
 
         return answer;
     }
